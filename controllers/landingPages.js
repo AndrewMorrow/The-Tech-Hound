0bb22d40f7237e82890873c31e5b207f1a676948
AndrewMorrow <beardedmongo@gmail.com>
2020-12-16 18:55:14 -0600

    sign up page added

--- controllers/landingPages.js
+++ controllers/landingPages.js
@@ -63,4 +63,15 @@ router.get("/login", (req, res) => {
     res.render("login");
 });
 
+// Login route
+router.get("/signup", (req, res) => {
+    // If the user is already logged in, redirect to the homepage
+    // if (req.session.loggedIn) {
+    //     res.redirect("/");
+    //     return;
+    // }
+    // Otherwise, render the 'login' template
+    res.render("sign-up");
+});
+
 module.exports = router;
